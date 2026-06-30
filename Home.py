67352829599ee0c18285743f554add96f1e86466
Mustafa Kamal <safoomalik47@gmail.com>
2026-06-30 12:19:32 +0500

    I have created a Vertual env

--- Home.py
+++ Home.py
@@ -1,6 +1,31 @@
-print("HelloWorld")
-a = 12
-b = 12
-print(a + b)
-print(a)
+# print("HelloWorld")
+# a = 12
+# b = 12
+# print(a + b)
+# print(a)
 #slkjfcksajnclksadc
+# x = None
+# print(x)
+# print(type(x))
+# x = 123
+# if x is None:
+#     print("no result yet...")
+# else:
+#     print("resiult is ready..")
+# x = None
+# if x is not None:
+#     print("Result is ready....")
+# else:
+#     print("result is not ready..")
+# print(bool(x))
+# y =True
+# while y == True:
+#     x = input("Enter a Number : ")
+#     try:
+#         num = int(x)
+#         y = False
+#         print("Thanks .")
+#     except:
+#         print("Enter a valid number....")
+
+
